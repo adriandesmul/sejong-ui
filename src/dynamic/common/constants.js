4bdb0fe5c9e66ba9e176a03208e585f3b9bac2b9
Herah Kim <herahkim@gmail.com>
2019-06-13 18:39:29 -0500

    Created About section, started new layout

--- src/dynamic/common/constants.js
+++ src/dynamic/common/constants.js
@@ -2,7 +2,7 @@ const countries = ['USA', 'Canada', 'Other'];
 const states = {
   'USA': ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'Other'],
   'Canada': ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'ON', 'PE', 'QC', 'SK'],
-  'Other': null
+  'Other':  ['AS', 'GU', 'MP', 'PR', 'VI']
 };
 const essayDivisions = ['Junior', 'Senior', 'Adult'];
 const juniorFolktales = ['Folktale A', 'Folktale B', 'Folktale C', 'Folktale D',
