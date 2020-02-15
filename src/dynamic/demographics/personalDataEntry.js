28b5e9f1dcd399c5c9e33f07a6ac24d6edabb71d
adriandesmul <adriandesmul@gmail.com>
2020-02-15 16:42:05 -0500

    Fixed null demo return

--- src/dynamic/demographics/personalDataEntry.js
+++ src/dynamic/demographics/personalDataEntry.js
@@ -22,6 +22,7 @@ export default function PersonalDataEntry() {
 
   useEffect(() => {
     API.get("/demographics", (err, data) => {
+      if (!data) return;
       let combinedData = {};
       for (const key in demographics) {
         combinedData[key] = data[key] || demographics[key];
@@ -62,126 +63,122 @@ export default function PersonalDataEntry() {
         <p>Personal information</p>
         {unsavedChanges && <p className="unsaved">Unsaved changes</p>}
       </div>
-      <div>
-        {msg.body && (
-          <div className={classNames(["scs-message", msg.type])}>
-            {msg.body}
-          </div>
-        )}
-        <div className="scs-module-element">
-          <label>First Name:</label>
-          <input
-            type="text"
-            name="personal_first_name"
-            value={demographics.personal_first_name}
-            placeholder="First name"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="scs-module-element">
-          <label>Last Name:</label>
-          <input
-            type="text"
-            name="personal_last_name"
-            value={demographics.personal_last_name}
-            placeholder="Last name"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="scs-module-element">
-          <label>Date of Birth:</label>
-          <input
-            type="number"
-            name="personal_date_of_birth_month"
-            value={demographics.personal_date_of_birth_month}
-            placeholder="MM"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-          &nbsp;/&nbsp;
-          <input
-            type="number"
-            name="personal_date_of_birth_day"
-            value={demographics.personal_date_of_birth_day}
-            placeholder="DD"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-          &nbsp;/&nbsp;
-          <input
-            type="number"
-            name="personal_date_of_birth_year"
-            value={demographics.personal_date_of_birth_year}
-            placeholder="YYYY"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="scs-module-element">
-          <label>Address:</label>
-          <input
-            type="text"
-            name="address_line_1"
-            value={demographics.address_line_1}
-            placeholder="Address 1"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="address_line_2"
-            value={demographics.address_line_2}
-            placeholder="Address 2"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="scs-module-element">
-          <label>Address:</label>
-          <input
-            type="text"
-            name="address_town"
-            value={demographics.address_town}
-            placeholder="Town"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="address_state"
-            value={demographics.address_state}
-            placeholder="State"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="address_country"
-            value={demographics.address_country}
-            placeholder="Country"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="address_zip"
-            value={demographics.address_zip}
-            placeholder="Zip Code"
-            className="scs-input"
-            onChange={handleInputChange}
-          />
-        </div>
-        <a
-          className="scs-button save"
-          onClick={() => {
-            handleSave();
-          }}
-        >
-          Save
-        </a>
+      {msg.body && (
+        <div className={classNames(["scs-message", msg.type])}>{msg.body}</div>
+      )}
+      <div className="scs-module-element">
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="personal_first_name"
+          value={demographics.personal_first_name}
+          placeholder="First name"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
       </div>
+      <div className="scs-module-element">
+        <label>Last Name:</label>
+        <input
+          type="text"
+          name="personal_last_name"
+          value={demographics.personal_last_name}
+          placeholder="Last name"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="scs-module-element">
+        <label>Date of Birth:</label>
+        <input
+          type="number"
+          name="personal_date_of_birth_month"
+          value={demographics.personal_date_of_birth_month}
+          placeholder="MM"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+        &nbsp;/&nbsp;
+        <input
+          type="number"
+          name="personal_date_of_birth_day"
+          value={demographics.personal_date_of_birth_day}
+          placeholder="DD"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+        &nbsp;/&nbsp;
+        <input
+          type="number"
+          name="personal_date_of_birth_year"
+          value={demographics.personal_date_of_birth_year}
+          placeholder="YYYY"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="scs-module-element">
+        <label>Address:</label>
+        <input
+          type="text"
+          name="address_line_1"
+          value={demographics.address_line_1}
+          placeholder="Address 1"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="address_line_2"
+          value={demographics.address_line_2}
+          placeholder="Address 2"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="scs-module-element">
+        <label>Address:</label>
+        <input
+          type="text"
+          name="address_town"
+          value={demographics.address_town}
+          placeholder="Town"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="address_state"
+          value={demographics.address_state}
+          placeholder="State"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="address_country"
+          value={demographics.address_country}
+          placeholder="Country"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="address_zip"
+          value={demographics.address_zip}
+          placeholder="Zip Code"
+          className="scs-input"
+          onChange={handleInputChange}
+        />
+      </div>
+      <a
+        className="scs-button save"
+        onClick={() => {
+          handleSave();
+        }}
+      >
+        Save
+      </a>
     </div>
   );
 }
