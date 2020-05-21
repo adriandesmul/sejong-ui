95d28fdce6a69c118d17a34cd8fb8279d9e1d9bf
adriandesmul <adriandesmul@gmail.com>
2020-05-20 19:33:57 -0500

    Fixed entry bug

--- src/dynamic/writingEntry/entryArea.js
+++ src/dynamic/writingEntry/entryArea.js
@@ -1,42 +1,41 @@
-import React from 'react';
-import ReactQuill from 'react-quill';
+import React from "react";
+import ReactQuill from "react-quill";
 
 class EntryArea extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' }
+    this.state = { text: "" };
     this.handleChange = this.handleChange.bind(this);
     this.modules = {
-      toolbar: [
-        ['bold', 'italic', 'underline'],
-        ['clean']
-      ]
-    }
-    this.formats = ['bold', 'italic', 'underline'];
+      toolbar: [["bold", "italic", "underline"], ["clean"]],
+    };
+    this.formats = ["bold", "italic", "underline"];
 
-    if (this.props.type === 'sijo') {
-      this.modules.toolbar[0].push({'indent': '-1'})
-      this.modules.toolbar[0].push({'indent': '+1'})
-      this.formats.push('indent')
+    if (this.props.type === "sijo") {
+      this.modules.toolbar[0].push({ indent: "-1" });
+      this.modules.toolbar[0].push({ indent: "+1" });
+      this.formats.push("indent");
     }
   }
 
   componentDidMount() {
-    this.setState({ text: this.props.initialValue })
+    this.setState({ text: this.props.initialValue || "" });
   }
 
   handleChange(value) {
     this.setState({ text: value });
-    this.props.onChange(this.state.text)
+    this.props.onChange(value);
   }
 
   render() {
     return (
-      <ReactQuill value={this.state.text}
-                  onChange={this.handleChange}
-                  modules={this.modules}
-                  formats={this.formats} />
-    )
+      <ReactQuill
+        value={this.state.text}
+        onChange={this.handleChange}
+        modules={this.modules}
+        formats={this.formats}
+      />
+    );
   }
 }
 
