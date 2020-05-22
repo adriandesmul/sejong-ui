f1d8ff47b4c19c58e8941afca9ed9050bcf10e0b
adriandesmul <adriandesmul@gmail.com>
2020-05-21 19:12:16 -0500

    Killing console logs

--- src/dynamic/past/winnersWriting.js
+++ src/dynamic/past/winnersWriting.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React from "react";
 
 class WinnersWritingTable extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			selectedYear: '2019'
-		}
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedYear: "2019",
+    };
 
-		this.handleClick = this.handleClick.bind(this)
-	}
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-	handleClick(e) {
-		console.log(e.target)
-		this.setState({selectedYear: e.target.id})
-	}
+  handleClick(e) {
+    this.setState({ selectedYear: e.target.id });
+  }
 
-	render() {
-		return (
-  			<div>
-				<p>
-					<a onClick={handleClick} href='#' id='2019'>2019</a> |
-					<a onClick={handleClick} href='#' id='2018'>2018</a> |
-					<a onClick={handleClick} href='#' id='2017'>2017</a>
-				</p>
-				<p>{this.state.selectedYear}</p>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <p>
+          <a onClick={handleClick} href="#" id="2019">
+            2019
+          </a>{" "}
+          |
+          <a onClick={handleClick} href="#" id="2018">
+            2018
+          </a>{" "}
+          |
+          <a onClick={handleClick} href="#" id="2017">
+            2017
+          </a>
+        </p>
+        <p>{this.state.selectedYear}</p>
+      </div>
+    );
+  }
 }
 
 export default WinnersWritingTable;
